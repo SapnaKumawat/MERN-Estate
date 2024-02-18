f32419669fbff1b89dc0ded6159146a97ef1a01d
Sapna <kumawatsapna199@gmail.com>
2024-02-18 11:14:06 +0530

    Created Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,21 @@
-import { BrowserRouter, Route, Routes  } from 'react-router-dom';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Profile from './Pages/Profile';
-import SignIn from './Pages/SignIn';
-import SignUp from './Pages/SignUp';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={< Home />} />
-        <Route path="/about" element={< About />} />
-        <Route path="/profile" element={< Profile />} />
-        <Route path="/signin" element={< SignIn />} />
-        <Route path="/signup" element={< SignUp />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
